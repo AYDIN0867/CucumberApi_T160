e58d60f7096d227783a7693966ee301f5522bf9f
Ramazan <coolstoves@gmail.com>
2025-07-12 23:33:49 +0300

    1207252333

--- src/test/java/stepdefinitions/JPHStepdefinitions.java
+++ src/test/java/stepdefinitions/JPHStepdefinitions.java
@@ -1,43 +1,59 @@
 package stepdefinitions;
 
 import io.cucumber.java.en.*;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import utilities.ConfigReader;
+
+import static org.junit.Assert.*;
 
 public class JPHStepdefinitions {
+    String url="";
+    Response response;
+    JsonPath resJP;
 
     @Given("kullanici {string} adresine gider")
-    public void kullanici_adresine_gider(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void kullanici_adresine_gider(String ConfigBaseurl) {
+        url+= ConfigReader.getProperty(ConfigBaseurl);
+
     }
     @Then("kullanici pathparametresi olarak {string} girer")
-    public void kullanici_pathparametresi_olarak_girer(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void kullanici_pathparametresi_olarak_girer(String pathParams) {
+
+    url=url+"/"+pathParams;
+
     }
     @Then("kullanici bir GET request yaparak donen response degerini kaydeder")
     public void kullanici_bir_get_request_yaparak_donen_response_degerini_kaydeder() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        response= RestAssured.given().when().get(url);
+
     }
     @Then("kullanici response statusCode degerinin {int} oldugunu dogrular")
-    public void kullanici_response_status_code_degerinin_oldugunu_dogrular(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void kullanici_response_status_code_degerinin_oldugunu_dogrular( int expstatsuCode) {
+
+        assertEquals(expstatsuCode,response.getStatusCode());
+
     }
     @Then("kullanici response content type degerinin {string} oldugunu dogrular")
-    public void kullanici_response_content_type_degerinin_oldugunu_dogrular(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void kullanici_response_content_type_degerinin_oldugunu_dogrular(String expContentType) {
+
+        assertEquals(expContentType,response.getContentType());
+
     }
     @Then("kullanici response degerini jsonPath olarak kaydeder")
     public void kullanici_response_degerini_json_path_olarak_kaydeder() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+       resJP=response.jsonPath();
+
     }
     @Then("kullanici response değerindeki {string} keyinin degerinin {string} oldugunu dogrular")
-    public void kullanici_response_değerindeki_keyinin_degerinin_oldugunu_dogrular(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void kullanici_response_değerindeki_keyinin_degerinin_oldugunu_dogrular(String expKey, String expValue) {
+
+        assertEquals(expValue,resJP.getString(expKey));
+
     }
 
 }
--- src/test/resources/features/JPH_GET_apiRequest.feature
+++ src/test/resources/features/JPH_GET_apiRequest.feature
@@ -7,7 +7,7 @@ Feature: JPH GET Request
   # "title" degerinin "optio dolor molestias sit" oldugunu test edin
 
 
-Scenario: JPH Get Request
+Scenario: US001_TC01_JPH_GetRequest
 
   Given kullanici "JPHUrl" adresine gider
   Then kullanici pathparametresi olarak "posts/44" girer
@@ -16,3 +16,36 @@ Scenario: JPH Get Request
   Then kullanici response content type degerinin "application/json; charset=utf-8" oldugunu dogrular
   Then kullanici response degerini jsonPath olarak kaydeder
   Then kullanici response değerindeki "title" keyinin degerinin "optio dolor molestias sit" oldugunu dogrular
+
+
+
+
+  # /*
+  #
+  #        https://jsonplaceholder.typicode.com/posts/70 url'ine  asagidaki body ile
+  #         bir POST request gonderdigimizde DONEN response'in
+  #         status code'unun 200,ve content type'inin application/json; charset=utf-8
+  #         Connection header degerinin "keep-alive"
+  #         ve response body'sinin asagida verilen ile ayni oldugunu test ediniz
+
+  #          Request Body                               Expected Data
+  #           {                                        {
+  #              "title":"Ahmet",                        "title":"Ahmet",
+  #              "body":"Merhaba",                        "body":"Merhaba",
+  #               "userId":10,                              "userId":10,
+  #               "id":70                                     "id":70
+  #              }                                          }
+  #
+  #      donen Response'un,
+  #
+  #
+  #      ve Response Body'sindeki,
+  #      "title" in "API" oldugunu
+  #      "UserId" degerinin 100 den kucuk oldugunu
+  #      "body" nin "API" kelimesi icerdigini test edin
+  #
+  #
+  #
+  #     */
+
+  #
